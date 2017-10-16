eaca439c4402ac2b37c0c59799fd2667b49e5931
HarshaBiyani <biyaniharsha2@gmail.com>
2017-10-16 15:45:18 +0530

    jhknkm

---
+++ app.py
@@ -0,0 +1,18 @@
+from __future__ import print_function
+from future.standard_library import install_aliases
+install_aliases()
+
+from flask import Flask, request, make_response
+import os
+app = Flask(__name__)
+
+@app.route('/')
+def homepage():
+    return "Welcome to API.AI"
+
+if __name__ == '__main__':
+    port = int(os.getenv('PORT', 5000))
+
+    print("Starting app on port %d" % port)
+
+    app.run(debug=False, port=port, host='0.0.0.0') 
