e202e956a63e5f5291f164e6f44626eb5385e037
HarshaBiyani <biyaniharsha2@gmail.com>
2017-10-16 16:00:04 +0530

    m

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from future.standard_library import install_aliases
 install_aliases()
 
 from flask import Flask, request, make_response
-import os
+import os, json
 app = Flask(__name__)
 
 @app.route('/')
@@ -13,6 +13,26 @@ def homepage():
 @app.route('/test')
 def homepage1():
     return "Test page"
+
+@app.route('/webhook',methods=["POST"])
+def homepage2():
+    req = request.get_json(silent=True, force=True)
+
+    print("Request:")
+    print(json.dumps(req, indent=4))
+    speech = "Hello"
+    res = {
+        "speech": speech,
+        "displayText": speech,
+        # "data": data,
+        # "contextOut": [],
+        "source": "apiai-weather-webhook-sample"
+        }
+    res = json.dumps(res, indent=4)
+    # print(res)
+    r = make_response(res)
+    r.headers['Content-Type'] = 'application/json'
+    return r
 
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 5000))
